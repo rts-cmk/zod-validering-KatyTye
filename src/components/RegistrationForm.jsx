46e229323316ccd4029896ebcf3a508aca175f5f
David Majholt <d.boy.dragoz@gmail.com>
2026-01-07 12:28:30 +0100

    style: added style for registration component

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -10,56 +10,56 @@ export default function RegistrationForm() {
 			</h2>
 
 			<label htmlFor="username" className="reg-form__field">
-				<span className="reg-form__field-text">Username</span>
+				<span className="reg-form__field-text">Username:</span>
 				<input type="text" name="username"
-					className="reg-form__field-input" autoComplete={"username"}></input>
+					className="reg-form__field-input" autoComplete={"false"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="firstname" className="reg-form__field">
-				<span className="reg-form__field-text">First name</span>
+				<span className="reg-form__field-text">First name:</span>
 				<input type="text" name="firstname"
 					className="reg-form__field-input" autoComplete={"given-name"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="lastname" className="reg-form__field">
-				<span className="reg-form__field-text">Last name</span>
+				<span className="reg-form__field-text">Last name:</span>
 				<input type="text" name="lastname"
 					className="reg-form__field-input" autoComplete={"family-name"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="email" className="reg-form__field">
-				<span className="reg-form__field-text">E-Mail</span>
+				<span className="reg-form__field-text">E-Mail:</span>
 				<input type="email" name="email"
 					className="reg-form__field-input" autoComplete={"email"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="password" className="reg-form__field">
-				<span className="reg-form__field-text">Password</span>
+				<span className="reg-form__field-text">Password:</span>
 				<input type="password" name="password"
 					className="reg-form__field-input" autoComplete={"new-password"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="repeatedPassword" className="reg-form__field">
-				<span className="reg-form__field-text">Repeat Password</span>
+				<span className="reg-form__field-text">Repeated Password:</span>
 				<input type="password" name="repeatedPassword"
 					className="reg-form__field-input" autoComplete={"new-password"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="birthday" className="reg-form__field">
-				<span className="reg-form__field-text">Birthday</span>
+				<span className="reg-form__field-text">Birthday:</span>
 				<input type="date" name="birthday"
 					className="reg-form__field-input" autoComplete={"bday-year"}></input>
 				<ul className="reg-form__field-errors"></ul>
 			</label>
 
 			<label htmlFor="phone" className="reg-form__field">
-				<span className="reg-form__field-text">Phonenumber</span>
+				<span className="reg-form__field-text">Phone Number:</span>
 				<input type="tel" name="phone"
 					className="reg-form__field-input" autoComplete={"tel"}></input>
 				<ul className="reg-form__field-errors"></ul>
@@ -70,6 +70,8 @@ export default function RegistrationForm() {
 					className="reg-form__field-input" autoComplete={"false"}></input>
 				<span className="reg-form__field-text">I accept to recive newsletters every month.</span>
 			</label>
+
+			<button type="submit" className="reg-form__submit">Create Account</button>
 		</form>
 	)
 }
